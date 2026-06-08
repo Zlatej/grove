6680eafce0a479c7774b9ccbd3bf6458bb9dbe64
Matej Skalicky <matej.skalickyy@gmail.com>
2026-06-09 01:46:34 +0200

    cfg

---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
+
+const GroveConfig = "grove/config.toml"
+
+type Config struct {
+	TicketIDPrefix string   `toml:"ticket_id_prefix"`
+	TicketIDFirst  bool     `toml:"ticket_id_first"`
+	BaseBranch     string   `toml:"base_branch"`
+	CloneWT        []string `toml:"clone_wt"`
+}
+
+func Load() (*Config, error) {
+	cfgPath, err := os.UserConfigDir()
+	if err != nil {
+		return nil, fmt.Errorf("getting config path: %w", err)
+	}
+
+	path := filepath.Join(cfgPath, GroveConfig)
+
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		if err = createNew(path); err != nil {
+			return nil, fmt.Errorf("creating new config: %w", err)
+		}
+	}
+
+	var cfg Config
+	if _, err := toml.DecodeFile(path, &cfg); err != nil {
+		return nil, fmt.Errorf("decoding config: %w", err)
+	}
+
+	return &cfg, nil
+}
+
+func createNew(path string) error {
+	strCfg := `# this is default config, adjust it to your needs
+
+# if you require to have the id of a ticket in branch name, uncomment and enter your ID prefix
+# ticket_id_prefix = "TID-" 
+ticket_id_first = true
+
+base_branch = "main"
+
+# worktrees created after a repository is cloned
+clone_wt = ["main", "review"]
+`
+	dirs, _ := strings.CutSuffix(path, "config.toml")
+	os.MkdirAll(dirs, 0755)
+	os.Create(path)
+
+	fmt.Println("creating new " + path)
+	return os.WriteFile(path, []byte(strCfg), 0755)
+}

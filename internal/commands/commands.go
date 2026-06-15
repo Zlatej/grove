40e72e90cd2d4d2a3e98f47fcfc64f7a8dd4f7d9
Matej Skalicky <matej.skalickyy@gmail.com>
2026-06-16 00:09:16 +0200

    commands handling

---
+++ internal/commands/commands.go
@@ -0,0 +1,71 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/zlatej/grove/internal/config"
+)
+
+// Choice contains information about one command
+type Choice struct {
+	Display string
+	Key     string
+	ID      CommandID
+}
+
+// Choices contain all implemented commands user can pick from
+var Choices = []Choice{
+	{"[N]ew", "n", New},
+	{"[D]elete", "d", Delete},
+	{"[U]pdate", "u", Update},
+	{"[C]lone", "c", Clone},
+	{"[L]ist", "l", List},
+}
+
+type CommandID string
+
+const (
+	New    CommandID = "new"
+	Delete CommandID = "delete"
+	Update CommandID = "update"
+	Clone  CommandID = "clone"
+	List   CommandID = "list"
+)
+
+type commandFunc func(cfg *config.Config) error
+
+var registry = map[CommandID]commandFunc{
+	New:    runNew,
+	Delete: runDelete,
+	Update: runUpdate,
+	Clone:  runClone,
+	List:   runList,
+}
+
+func RunCommand(cmdID CommandID, cfg *config.Config) error {
+	cmd, ok := registry[cmdID]
+	if !ok {
+		return fmt.Errorf("command with identifier %s does not exist", cmdID)
+	}
+	return cmd(cfg)
+}
+
+func runNew(cfg *config.Config) error {
+	return nil
+}
+
+func runDelete(cfg *config.Config) error {
+	return nil
+}
+
+func runUpdate(cfg *config.Config) error {
+	return nil
+}
+
+func runClone(cfg *config.Config) error {
+	return nil
+}
+
+func runList(cfg *config.Config) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -4,17 +4,28 @@ import (
 	"log/slog"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/zlatej/grove/internal/commands"
 	"github.com/zlatej/grove/internal/config"
 	"github.com/zlatej/grove/internal/ui"
 )
 
 func main() {
-	_, err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
-		slog.Error("loading config", "error", err.Error())
+		slog.Error("loading config", "error", err)
+		return
 	}
 	p := tea.NewProgram(ui.NewModel())
-	if _, err := p.Run(); err != nil {
+	tm, err := p.Run()
+	if err != nil {
 		slog.Error("running menu model", "error", err)
+		return
+	}
+	m, ok := tm.(ui.Model)
+	if !ok || len(m.Selected) == 0 {
+		return
+	}
+	if err := commands.RunCommand(m.Selected, cfg); err != nil {
+		slog.Error("executing command", "command", m.Selected, "error", err)
 	}
 }
